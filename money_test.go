d772e1ec8e37f975808ea36571c1d1a025191063
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-01-23 23:55:05 +0900

    Add test

--- money_test.go
+++ money_test.go
@@ -6,9 +6,14 @@ import (
 
 func TestMultiplication(t *testing.T) {
 	var five = Dollar{Amount: 5}
-	five.Times(2)
 
+	five.Times(2)
 	if 10 != five.Amount {
 		t.Errorf("five.amount should be 10")
+	}
+
+	five.Times(3)
+	if 15 != five.Amount {
+		t.Errorf("five.amount should be 15")
 	}
 }
